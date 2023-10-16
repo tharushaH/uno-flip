4525afa35b08f56cf078f4b7a1d3dfcb8a8da3b1
hubert <hubertdang@cmail.carleton.ca>
2023-10-16 14:38:35 -0400

    Created unit test for Deck class, DeckTest.java

---
+++ src/DeckTest.java
@@ -0,0 +1,23 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class DeckTest {
+    private Deck deck;
+    private final static int MAX_SIZE = 112;
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void testTakeCard() {
+    }
+
+    @Test
+    public void testPutCard() {
+    }
+
+    @Test
+    public void testResetDeck() {
+    }
+}
