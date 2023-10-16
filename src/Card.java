4a71ed714aa4bdda0b524f0106a6605ffb95040e
Tharusha Herath <tharusha.herath13@gmail.com>
2023-10-16 19:26:02 -0400

    Change variable rank and colour to final.

--- src/Card.java
+++ src/Card.java
@@ -2,8 +2,8 @@ public class Card {
     public enum Colour {RED, BLUE, YELLOW, GREEN, WILD};
     public enum Rank {ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, DRAW_ONE, REVERSE, SKIP, WILD, WILD_DRAW_2};
 
-    private Rank rank;
-    private Colour colour;
+    private final Rank rank;
+    private final Colour colour;
 
     public Card(Rank rank, Colour colour){
         this.rank = rank;
