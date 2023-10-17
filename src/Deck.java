8d6e187df5d02118c47ee2d2e2a4bc56255b3b08
hubert <hubertdang@cmail.carleton.ca>
2023-10-16 22:25:09 -0400

    Added a private constant for the max size of the deck.

--- src/Deck.java
+++ src/Deck.java
@@ -3,12 +3,14 @@ import java.util.Stack;
 public class Deck {
     private Stack<Card> deck;
     private Stack<Card> discard;
+    private static final int MAX_SIZE = 108;
 
     public Deck() {}
 
-    public Card takeCard(int numCards) {return null;}
+    public Card takeCard() {return null;}
 
-    public void putCard(Card card){}
+    public void putCard(Card card) {
+    }
 
     public int size() {
         return deck.size();
