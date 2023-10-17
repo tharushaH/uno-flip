48cc37360f4d61665e5fca4f96afc2ba9817c98d
tharu <tharusha.herath13@gmail.com>
2023-10-17 12:22:04 -0400

    Completed all function implementations for Hand and tested.

--- src/Hand.java
+++ src/Hand.java
@@ -4,18 +4,35 @@ import java.util.ArrayList;
 public class Hand {
     private ArrayList<Card> cards;
 
-    public Hand() {}
+    public Hand() {
+        this.cards = new ArrayList<Card>();
+    }
 
-    public void addCard(Card card) {}
+    public void addCard(Card card) {
+        this.cards.add(card);
+    }
 
-    public Card popCard(int index) {return null;}
+    public Card popCard(int index) {
+        if (index < 0 || index >= cards.size())
+            throw new IndexOutOfBoundsException("ERROR: Out of index.");
+
+        return this.cards.remove(index);
+    }
 
     public int size() {
-        return cards.size();
+        return this.cards.size();
     }
 
     @Override
-    public String toString() {return null;}
+    public String toString() {
+        StringBuilder message = new StringBuilder();
+        for (int i = 0; i < cards.size(); i++){
+            message.append(i + 1).append(". ").append(cards.get(i)).append("\n");
+        }
+        return message.toString();
+    }
 
-    public ArrayList<Card> getCards(){return null;}
+    public ArrayList<Card> getCards(){
+        return this.cards;
+    }
 }
