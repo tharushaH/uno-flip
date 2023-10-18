fb1a80e330f2b3b77afdcce49e38fcc9c4417a56
hubert <hubertdang@cmail.carleton.ca>
2023-10-18 16:06:06 -0400

    Create and implement TurnSequence class. This class is abstract and will be used to handle game sequences by changing the game state.

---
+++ src/TurnSequence.java
@@ -0,0 +1,23 @@
+/**
+ * TurnSequence is an abstract class to be extended in order to handle game sequences based on the card played.
+ */
+public abstract class TurnSequence {
+    private Game game;
+
+    /**
+     * Executes the appropriate game sequence according to the card played by changing the game state.
+     */
+    public abstract void executeSequence();
+
+    /**
+     * Checks if the card to play is a valid card to play according to Uno Flip! rules.
+     * @param card The card to play
+     * @return true if the card is valid, false otherwise
+     */
+    public boolean isValid(Card card) {
+        if (card.isWild()) {
+            return true;
+        }
+        return game.getCurrentRank() == card.getRank() || game.getCurrentColour() == card.getColour();
+    }
+}
