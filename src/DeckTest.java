44c0b2b67f7731885439b8249b3b2bed996707aa
hubert <hubertdang@cmail.carleton.ca>
2023-10-17 21:14:20 -0400

    Bugfix testTakeCardFromEmptyDeck()

--- src/DeckTest.java
+++ src/DeckTest.java
@@ -28,25 +28,28 @@ public class DeckTest {
     @Test
     public void testTakeCardFromEmptyDeck() {
         // empty out the deck by taking all the cards
-        for (int i = 0; i < (MAX_SIZE ); i++) {
+        for (int i = 0; i < MAX_SIZE; i++) {
             deck.takeCard();
         }
         assertEquals(0, deck.size());    // discard pile has nothing to make new deck with, should be 0
 
         deck = new Deck();
 
-        // deck is max size now, take more cards than the deck has
-        for (int i = 0; i < (MAX_SIZE + 2); i++) {
+        // empty out deck
+        for (int i = 0; i < MAX_SIZE; i++) {
             deck.takeCard();
         }
 
-        // put some cards in discard to make a new deck with
+        // put some cards in discard
         deck.putCard(new Card(Card.Rank.REVERSE, Card.Colour.RED));
         deck.putCard(new Card(Card.Rank.THREE, Card.Colour.BLUE));
         deck.putCard(new Card(Card.Rank.THREE, Card.Colour.BLUE));
         deck.putCard(new Card(Card.Rank.WILD, Card.Colour.WILD));
 
-        assertEquals(2, deck.size());    // deck should reset with 4 cards, but 2 more were taken after
+        deck.takeCard();    // take 2 cards even though deck is empty
+        deck.takeCard();
+
+        assertEquals(2, deck.size());    // deck should reset with 4 cards, but 2 more were taken after*/
     }
 
     @Test
