831021f24e08dd53f2131aa65cfc0048b5e6785b
Tharusha Herath <tharusha.herath13@gmail.com>
2023-10-18 23:12:38 -0400

    Added Javadocs for Card and Hand Class.

--- src/Card.java
+++ src/Card.java
@@ -1,5 +1,15 @@
 import java.awt.*;
 
+
+/**
+ * The Card class represents a card used in the Uno Flip game. Each card has a color and a rank.
+ * It provides methods for checking wild, getting the rank and colour of the card, and generating a string
+ * representation of the hand.
+ *
+ * Author: Tharusha Herath
+ * Date: 2023-10-18
+ */
+
 public class Card {
     public enum Colour {RED, BLUE, YELLOW, GREEN, WILD};
     public enum Rank {ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, DRAW_ONE, REVERSE, SKIP, WILD, WILD_DRAW_2};
@@ -7,6 +17,13 @@ public class Card {
     private final Rank rank;
     private final Colour colour;
 
+    /**
+     * Constructs a Card object with the specified rank and color.
+     *
+     * @param rank   the rank of the card
+     * @param colour the color of the card
+     * @throws IllegalArgumentException if an invalid card is attempted to be created
+     */
     public Card(Rank rank, Colour colour){
         if ((rank == Rank.WILD || rank == Rank.WILD_DRAW_2) && !(colour == Colour.WILD))
             throw new IllegalArgumentException("ERROR: Invalid Card Creation.");
@@ -16,18 +33,37 @@ public class Card {
         this.colour = colour;
     }
 
+    /**
+     * Checks if the card is a wild card.
+     *
+     * @return true if the card is a wild card, false otherwise
+     */
     public boolean isWild(){
         return this.colour == Colour.WILD;
     }
 
+    /**
+     * Returns a Colour, for the colour of the card.
+     * @return returns the colour of the Card.
+     */
     public Colour getColour() {
         return this.colour;
     }
 
+    /**
+     * Checks if the card is a wild card.
+     *
+     * @return true if the card is a wild card, false otherwise
+     */
     public Rank getRank(){
         return this.rank;
     }
 
+    /**
+     * Returns a string representation of the card, displaying its color followed by its rank.
+     *
+     * @return the string representation of the card
+     */
     @Override
     public String toString(){
         if (this.rank == Rank.WILD_DRAW_2)
@@ -39,6 +75,12 @@ public class Card {
         return this.colour + " " + this.rank;
     }
 
+    /**
+     * Checks if this card is equal to another object.
+     *
+     * @param obj the object to compare with this card
+     * @return true if the cards are equal, false otherwise
+     */
     @Override
     public boolean equals(Object obj){
         if (obj == null)
--- src/Hand.java
+++ src/Hand.java
@@ -1,17 +1,41 @@
 import java.awt.*;
 import java.util.ArrayList;
 
+/**
+ * The Hand class represents a collection of Card objects held by a player
+ * in a card game. It provides methods for adding cards to the hand, removing
+ * cards from the hand, getting the size of the hand, and generating a string
+ * representation of the hand.
+ *
+ * Author: Tharusha Herath
+ * Date: 2023-10-18
+ */
 public class Hand {
     private ArrayList<Card> cards;
 
+    /**
+     * Constructs an empty Hand object. Initializes an empty arraylist of cards.
+     */
     public Hand() {
         this.cards = new ArrayList<Card>();
     }
 
+    /**
+     * Adds a Card object to the hand.
+     *
+     * @param card the Card object to be added to the hand
+     */
     public void addCard(Card card) {
         this.cards.add(card);
     }
 
+    /**
+     * Removes and returns the Card object at the specified index from the hand.
+     *
+     * @param index the index of the Card object to be removed
+     * @return the Card object removed from the hand
+     * @throws IndexOutOfBoundsException if the index is out of range
+     */
     public Card popCard(int index) {
         if (index < 0 || index >= cards.size())
             throw new IndexOutOfBoundsException("ERROR: Out of index.");
@@ -19,10 +43,21 @@ public class Hand {
         return this.cards.remove(index);
     }
 
+    /**
+     * Returns the number of cards in the hand.
+     *
+     * @return the number of cards in the hand
+     */
     public int size() {
         return this.cards.size();
     }
 
+    /**
+     * Returns a string representation of the hand, including the index and
+     * string representation of each card in the hand.
+     *
+     * @return a string representation of the hand
+     */
     @Override
     public String toString() {
         StringBuilder message = new StringBuilder();
@@ -32,6 +67,11 @@ public class Hand {
         return message.toString();
     }
 
+    /**
+     * Gets the list of Card objects in the hand.
+     *
+     * @return an ArrayList of Card objects in the hand
+     */
     public ArrayList<Card> getCards(){
         return this.cards;
     }
