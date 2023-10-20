e835dd2ff3d67725ff44201edfe3ffa4d888df17
hubert <hubertdang@cmail.carleton.ca>
2023-10-19 22:37:23 -0400

    Created and implemented Wild class, a subcalss of TurnSequence that handles the Wild card.

---
+++ src/Wild.java
@@ -0,0 +1,14 @@
+public class Wild extends TurnSequence {
+    /**
+     * Executes the appropriate game sequence according to the card played by changing the game state.
+     *
+     * @param card The card that was played
+     */
+    @Override
+    public void executeSequence(Card card) {
+        game.setTopCard(card);
+        game.setCurrentColour(game.getColourSelectedByWild());
+        game.setCurrentRank(Card.Rank.WILD);
+        game.nextTurn();
+    }
+}
