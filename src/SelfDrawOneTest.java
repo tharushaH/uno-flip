14676b5b89fff9fd2079d948abca919ee5f00388
Tharusha Herath <tharusha.herath13@gmail.com>
2023-10-20 22:57:25 -0400

    Created file for testing SelfDrawOne.

---
+++ src/SelfDrawOneTest.java
@@ -0,0 +1,18 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class SelfDrawOneTest {
+    private SelfDrawOne selfDrawOne;
+    private Game game;
+
+    @Before
+    public void setUp(){
+        game = new Game();
+    }
+
+    @Test
+    public void testExecuteSequence(){
+        game.
+    }
+
+}
