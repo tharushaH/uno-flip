68b2577d70f3c041e2c60ce977ff803cd7ec0758
hubert <hubertdang@cmail.carleton.ca>
2023-11-04 13:57:03 -0400

    Added javadoc to handleUnoFlipStatusUpdate method.

--- src/UnoFlipView.java
+++ src/UnoFlipView.java
@@ -1,3 +1,9 @@
 public interface UnoFlipView {
+
+    /**
+     * Handles a UnoFlipEvent object by updating the view.
+     *
+     * @param e
+     */
     public void handleUnoFlipStatusUpdate(UnoFlipEvent e);
 }
