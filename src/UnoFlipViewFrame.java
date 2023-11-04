d59cc5779c77e012253ff9006e8d9eb85e17eb6e
hubert <hubertdang@cmail.carleton.ca>
2023-11-04 13:53:43 -0400

    Created UnoFlipViewFrame class. This class implements UnoFlipView and is the main frame of the game's GUI.

---
+++ src/UnoFlipViewFrame.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class UnoFlipViewFrame extends JFrame implements UnoFlipView {
+    public UnoFlipViewFrame() {}
+
+    /**
+     * Handles a UnoFlipEvent object by updating the view.
+     *
+     * @param e
+     */
+    @Override
+    public void handleUnoFlipStatusUpdate(UnoFlipEvent e) {
+    }
+}
