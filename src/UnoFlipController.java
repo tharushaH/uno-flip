b0d1bd9fd8c36f36a90315d35a0c99fc536df562
tharu <tharusha.herath13@gmail.com>
2023-11-13 12:49:39 -0500

    Added JOptionPane for controller.

--- src/UnoFlipController.java
+++ src/UnoFlipController.java
@@ -1,22 +1,70 @@
+import javax.swing.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
+
+/**
+ * The UnoFlipController class implements the ActionListener interface
+ * to handle user interactions and events in the Uno Flip game.
+ */
 public class UnoFlipController implements ActionListener {
     private UnoFlipModel model;
 
+    /**
+     * Constructs a UnoFlipController with the specified UnoFlipModel.
+     *
+     * @param model The UnoFlipModel instance associated with the controller.
+     */
     public UnoFlipController(UnoFlipModel model){
         this.model = model;
     }
 
 
+
+    /**
+     * Invoked when an action occurs. Handles various actions triggered
+     * by user interactions with the Uno Flip game.
+     *
+     * @param e The ActionEvent representing the user's action.
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand(UnoFlipView.START_CMD)){
+
+            String[] numPlayerOptions = {"2 Players", "3 Players", "4 Players"};
+
+            JComboBox<String> comboBox = new JComboBox<>(numPlayerOptions);
+
+            int result = JOptionPane.showOptionDialog(
+                    null,
+                    comboBox,
+                    "Select number of players:",
+                    JOptionPane.DEFAULT_OPTION,
+                    JOptionPane.PLAIN_MESSAGE,
+                    null,
+                    null,
+                    null);
+
+            if (result == JOptionPane.OK_OPTION) {
+                String option = (String) comboBox.getSelectedItem();
+                int numPlayers = Integer.parseInt(option);
+            } else {
+                System.exit(0); // absolutely must select an option, otherwise, do not start the game
+            }
+
+            for (int i = 0; i < numPlayers; i++){
+                String name = (String) JOptionPane.showInputDialog("ENTER AMOUNT OF PLAYERS");
+                this.model.setPlayer(name);
+            }
+            this.model.setUpIntialTopCard();
+        }
         if(e.getActionCommand().equals("draw")){
             model.playTurn(-1);
         }
         if(e.getActionCommand().equals("next")) {
             model.nextTurn();
         }
+
 
         try{
             int index = Integer.parseInt(e.getActionCommand());
