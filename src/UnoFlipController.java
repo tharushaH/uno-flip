cae7952fde318a4d1d0547b58253a5ccaa649305
tharu <tharusha.herath13@gmail.com>
2023-11-14 17:05:39 -0500

    Continues to next JOptionPane and defaults name to 'player + playernumber'.

--- src/UnoFlipController.java
+++ src/UnoFlipController.java
@@ -64,7 +64,7 @@ public class UnoFlipController implements ActionListener {
 
             for (int i = 0; i < numPlayers; i++){
                 String name = (String) JOptionPane.showInputDialog("Enter player " + Integer.toString(i+1) + " name");
-                if (name.equals("")){
+                if (name == null || name.equals("")){
                     this.model.createPlayer("Player " + (i+1));
                 } else{
                     this.model.createPlayer(name);
